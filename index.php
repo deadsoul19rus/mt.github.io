f323ba6c97aa2ec86bdba9db2d66f897a5d00f5a
MaximTodyshev <deadone19rus@gmail.com>
2018-03-30 18:59:37 +0700

    files

---
+++ index.php
@@ -0,0 +1,279 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0">
+		<meta charset="UTF-8">
+		<title>Тестовое задание</title>
+		<script type="text/javascript"></script>
+		<link href="styles.css" rel="stylesheet" type="text/css">	
+		<link href="https://fonts.googleapis.com/css?family=PT+Sans:200,400,500,600,700" rel="stylesheet">
+		
+	</head>
+		
+	<body>
+		<div class="container">
+			<div class="flx">
+				<div class="logo_top">
+					<img src="images/logo.png">
+				</div>
+				<div class="vis">
+					<div>
+						<a href="#">
+							Личный офис
+						</a>
+						<a href="#">
+							Система работы
+						</a>
+						<p>
+							Твой ID: <span>007-1364242</span>
+							
+						</p>
+						<p>
+							Наставник: <span>007-1323151, Горохова&nbsp;Оксана</span>
+							
+						</p>
+						<p>
+							Телефон наставника: <span>7&#8209;911&#8209;882&#8209;13&#8209;46</span>
+							
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="telegram_bot">
+				<div class="row1">
+					<img class="logo_nl_tb" src="images/logo.png">
+					<div class="text-telegram">
+						<p class="hello">	
+							Привет, Мария!
+						</p>
+						<p class="add">
+							Подпишись<br>
+							на&nbspЧат&#8209Бот NL_Assistant 
+							в&nbspTelegram!
+						</p>
+						<a class="btn">
+							Присоединяйся!
+						</a>
+					
+					</div>
+				</div>	
+				<div class="row2">
+					<img class="iph" src="images/iPhone.png">
+					<img src="images/Telegram.png" class="logo_tel" >
+				</div>
+			</div>
+			<div  class="text_block">
+				<span class="boring-text">
+					Будь в курсе всего происходящего в компании и получи 
+					персонального помощника в своем телефоне!
+					Ботом пользуются уже более 30 тысяч менеджеров 
+					и клиентов компании  NL.
+				</span>
+				<span class="rage-text">
+					С Чат-ботом NL у тебя под рукой <br>
+					всегда будут ответы на вопросы.
+				</span>
+				<span class="boring-text">
+					Ты выбираешь, какая информация тебя интересует, <br>
+					бот выдает готовый ответ.
+				</span>
+				<span class="boring-text">
+					Наш дружелюбный умный бот в Телеграм делится с менеджерами 
+					и клиентами тем, что знает.	
+				</span>
+				<img class="book" src="images/book.png">
+			
+			</div>
+			<div class="find_telegr">
+				<div class="flx">
+					<div class="first_iph">
+						<img src="images/iPhone.png">
+					</div>
+					<div class="second_descr">
+						<p class="find_in">
+							В нем ты найдешь:
+						</p>
+						<span class="f_i">
+							<span>График событий,</span> чтобы всегда быть в курсе важных событий компании;
+						</span>
+						<span class="f_i">
+							<span>Продукты</span> и самую важную информацию о них, а еще видео и частые вопросы;
+						</span>
+						<span class="f_i">
+							<span>Программы</span> питания для разных целей + меню и рекомендации;
+						</span>
+						<span class="f_i mrg">
+							<span>Бизнес:</span>
+						</span>
+						<ul class="biss">
+							<li>Моментальный просмотр ГО&nbspи&nbspЛО,</li>
+							<li>Регистрация менеджера или&nbspклиента,</li>
+							<li>Рефссылки и другие полезные&nbspштуки,</li>
+							<li>Баланс счетов.</li>
+						</ul>
+					</div>
+				</div>
+				<div class="nice_border">
+					<span>
+						Плюс информация о магазинах, партнерах, проектах, сервисах и др.
+					</span>
+				</div>
+				<div>
+					Поэтому бот - это дополнительный канал быстрого<br>
+					и емкого консультирования тебя и твоих клиентов.
+				</div>
+				<img class="cup" src="images/cup.png">
+			</div>
+			<div class="manual">
+				<div class="rage-text">
+					Ты еще не подключился?<br>
+					Значит, эта инструкция для тебя.
+				</div>
+				<span class="f_i">
+							<span>Как подключиться?</span>
+				</span>
+				<ul class="ul_how">
+					<li>Установи в своем телефоне приложение Telegram.</li>
+					<li>Пройди по ссылке <a href="#">@nl_assistant_bot</a> и подпишись.</li>	
+					<li>Или найди в поисковой строке чатов адрес<br>@NL_Assistant и подпишись.</li>
+					<li>Чтобы тебе был доступен самый интересный и полный фунцкионал, авторизируйся.</li>
+				</ul>
+				<div>
+					Все! Ты в чат-боте.
+				</div>
+				<p>
+					Бот доступен абсолютно для всех!
+				</p>
+				<p>
+					Устанавливай себе и отправляй ссылку своим клиентам.
+				</p>
+			</div>
+			<div class="dwnld">
+				<div class="d1">
+					<div class="nl_store befico">
+						<div class="mrgst">	
+							<span class="title st">
+								NL Store
+							</span>
+							<div class="fz">
+								Интернет-магазин<br>
+								в твоем мобильном
+							</div>
+						</div>	
+						<div class="d4m">
+							<a href="#" class="dlink"> 
+								<img src="images/app.png">							
+							</a>
+							<a href="#" class="dlink"> 
+								<img src="images/qw.png">
+							</a>
+						</div>
+					</div>
+					<div class="nl_reader befico">
+						<div class="mrgst">	
+							<span class="title rd">
+								NL Reader
+							</span>
+							<div class="fz">
+								Коммуникационное<br>
+								приложение
+							</div>
+						</div>	
+						<div class="d4m">
+							<a href="#" class="dlink"> 
+								<img src="images/app.png">							
+							</a>
+							<a href="#" class="dlink"> 
+								<img src="images/qw.png">
+							</a>
+						</div>
+					</div>
+				</div>
+				
+			</div>
+			<div class="sbr">
+				<div class="d1">
+					<div class="befico nl_news">
+						<div class="mrgst">	
+							<span class="title st">
+								NL_News
+							</span>
+							<div>
+								Узнавай новости<br>
+								первым в Telegram
+							</div>
+						</div>
+						<a href="#" class="btn_sub">
+							Подписаться
+						</a>
+					</div>
+					<div class="befico nl_assist">
+						<div class="mrgst">	
+							<span class="title st">
+								NL_Assistant
+							</span>
+							<div>
+								Твой надежный<br>
+								помощник в Telegram
+							</div>
+						</div>
+						<a href="#" class="btn_sub">
+							Подписаться
+						</a>
+					</div>
+				</div>
+			</div>
+			
+			<div class="dwnld pd">
+				<div class="cent">
+					<div class="befico build">
+							<div class="mrgst">	
+								<span class="title st">
+									NeBoSkreb
+								</span>
+								<div>
+									Обучающая игра<br>
+									об ассортименте
+								</div>
+							</div>
+							
+					</div>
+				</div>
+				<div class="cent">
+						<a href="#" class="dlink"> 
+							<img src="images/app.png">							
+						</a>
+						<a href="#" class="dlink"> 
+							<img src="images/qw.png">
+						</a>
+				</div>
+			</div>
+			
+			<div class="sbr fnt">
+				<div class="z_h3">
+					Присоединяйся к нам<br>в&nbspсоцсетях
+				</div>
+				<div class="socials">
+					<a class="s_item vk_ic" href="#">
+						Вконтакте
+					</a>
+					<a class="s_item inst_ic" href="#">
+						Instagram
+					</a>
+					<a class="s_item yt_ic" href="#">
+						Youtube
+					</a>
+				</div>
+				<div class="msg">
+					Вы получили это письмо, так как подписались на рассылку<br> на <a href="#" class="undrl">нашем сайте</a> 
+					телефон горячей линии: <a href="tel:+78002500800">8-800-2-500-800</a>
+				</div>
+				
+			</div>
+			
+		</div>
+		
+	
+	</body>
+
+</html>
